b40188b41dbf929e028626ac1843bd950f1a5c90
sophiea0110 <sophiea0110@naver.com>
2021-11-21 00:13:06 +0900

    To do 구현

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,20 @@
-const weathers = ["☁", "⛅", "🌩", "⛈", "🌤", "🌥", "🌦", "🌧", "🌨", "🌩"]
+const weathers = ["☁", "⛅", "🌧"]
 const API_KEY = "9c3640eecac9f7bffab0aeba3753d0c2"
 
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const log = position.coords.longitude;
+    const lang = "KR"
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=matric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric&lang=${lang}`
     fetch(url).then(response => response.json())
     .then(data => {
-        const weather = document.querySelector("#weather span:first-child")
-        const city = document.querySelector("#weather span:last-child")
+        const city = document.querySelector("#weather span:first-child")
+        const weather = document.querySelector("#weather span:last-child")
+    
         city.innerText = data.name
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+        weather.innerText = `${data.weather[0].description}     ${data.main.temp}º`
     })
 }
 function onGeoError(){
